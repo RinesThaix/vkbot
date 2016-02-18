1523cbdc79f2f9c1b30bbe42536c62b4a27d1223
Валерий <faerytea@gmail.com>
2016-02-18 20:36:00 +0300

    Add flag 'nocaps' to MemeCreate

--- src/main/java/ru/ifmo/vkbot/utils/ImagesWorker.java
+++ src/main/java/ru/ifmo/vkbot/utils/ImagesWorker.java
@@ -90,11 +90,7 @@ public class ImagesWorker {
         return 0;
     }
 
-    private static File addTextAndSave(BufferedImage image, String name, String ext, String textU, String textD) throws IOException {
-        textU = textU.toUpperCase();
-        textD = textD.toUpperCase();
-
-        Graphics g = image.getGraphics();
+    private static File addTextAndSave(BufferedImage image, String name, String ext, String textU, String textD) throws IOException {Graphics g = image.getGraphics();
         Font f = new Font("Impact", Font.BOLD, 60);
         g.setFont(f);
         float size = 60f;
