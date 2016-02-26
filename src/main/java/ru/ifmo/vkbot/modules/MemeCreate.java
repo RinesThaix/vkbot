84bd065a2e494126040f740bbbf2ff6567cc8f5f
Валерий <faerytea@gmail.com>
2016-02-27 00:15:08 +0300

    Fix for uppercase in top line in memes

--- src/main/java/ru/ifmo/vkbot/modules/MemeCreate.java
+++ src/main/java/ru/ifmo/vkbot/modules/MemeCreate.java
@@ -22,7 +22,7 @@ public class MemeCreate extends BotModule {
         // говнокод
         boolean caps = "capsoff".equals(args[args.length - 1].toLowerCase()) || "некапс".equals(args[args.length - 1].toLowerCase());
         if(caps) {
-            args = Arrays.copyOf(args, args.length - 1); // Мне лень разбираться в коде дальше, поэтому я просто оставлю это так
+            args = Arrays.copyOf(args, args.length - 2); // Мне лень разбираться в коде дальше, поэтому я просто оставлю это так
         } else {
             if(args.length >= 2) {                       // Вынес апперкейс сюда
                 args[args.length - 1] = args[args.length - 1].toUpperCase();
