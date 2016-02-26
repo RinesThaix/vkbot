0b7ed47aadf9764349d839517ab457393f27483c
Валерий <faerytea@gmail.com>
2016-02-27 01:49:12 +0300

    Add 'короче' and countOfResults Теперь может парсить запросы типа "поиск 10 короче запрос" - выдаст 10 результатов без пояснения.

--- src/main/java/ru/ifmo/vkbot/modules/Find.java
+++ src/main/java/ru/ifmo/vkbot/modules/Find.java
@@ -14,6 +14,8 @@ import ru.ifmo.vkbot.utils.PostExecutor;
 @SuppressWarnings("deprecation")
 public class Find extends BotModule {
 
+    private static final byte MAX_SEARCH_RESULTS = 50;
+
     public Find(VkBot vkbot) {
         super(vkbot);
     }
@@ -24,12 +26,30 @@ public class Find extends BotModule {
             getMC().sendAttached(m.getDialog(), "Недостаточно аргументов!", m.getMessageId());
             return;
         }
+        final boolean shorter;
+        final byte countOfResults;
         StringBuilder query = new StringBuilder();
-        for(String s : args)
-            query.append(s).append(" ");
+        {    /// Парсинг трешака
+            int index = 0;
+            byte t = 0;
+            if (Character.isDigit(args[index].charAt(0))) {
+                try {
+                    t = Byte.decode(args[index]);
+                    ++index;
+                } catch (NumberFormatException e) {
+                    t = 0;
+                }
+                if (t > MAX_SEARCH_RESULTS) t = MAX_SEARCH_RESULTS;
+            }
+            countOfResults = t;
+            if (shorter = args[index].equalsIgnoreCase("короче"))
+                ++index;
+            // То, что осталось - запрос
+            for (; index < args.length; ++index)
+                query.append(args[index]).append(" ");
+        }
         String s = query.toString();
-        s = s.substring(0, s.length() - 1);
-        s = PostExecutor.encode(s);
+        s = PostExecutor.encode(s.substring(0, s.length() - 1));
         String answer = PostExecutor.executeGet("http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=" + s);
         try {
             StringBuilder sb = new StringBuilder();
@@ -41,13 +61,14 @@ public class Find extends BotModule {
             }
             JSONArray array = (JSONArray) json.get("results");
             sb.append("Вот, что мне удалось найти:\n");
-            for(int i = 0; i < Math.min(3, array.size()); ++i) {
+            for(int i = 0; i < Math.min(countOfResults, array.size()); ++i) {
                 json = (JSONObject) array.get(i);
                 String url = (String) json.get("url");
                 String title = (String) json.get("titleNoFormatting");
                 String description = (String) json.get("content");
-                sb.append("(").append(i + 1).append(") ").append(title).append("\n")
-                        .append("Ссылка: ").append(url).append("\n").append(description).append("\n\n");
+                sb.append("(").append(i + 1).append(") ").append(title).append('\n');
+                if (!shorter) sb.append("Ссылка: ").append(url).append('\n').append(description).append("\n\n");
+                else sb.append('\t').append(url).append('\n');
             }
             getMC().sendAttached(m.getDialog(), sb.toString().replaceAll("<[^>]*>", ""), m.getMessageId());
         }catch(Exception ex) {
